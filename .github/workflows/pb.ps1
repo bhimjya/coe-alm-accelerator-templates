58cef06dd7af88657377034fa214c2d7aa2c404d
bhimjya <bhimjyanip@gmail.com>
2024-07-21 21:06:07 +0530

    Update pb.ps1

--- .github/workflows/pb.ps1
+++ .github/workflows/pb.ps1
@@ -1,3 +1,4 @@
 Write-Host "Hello, World!"
 Get-Date
-Copy-Item ".github\\workflows\\pr-loop.ps1" -Destination ".github\\pm"
+Copy-Item ".\\github\\workflows\\pr-loop.ps1" -Destination ".github\\ISSUE_TEMPLATE"
+Write-Host "done"
