fb54d01103b5f2a37bec1b24be93f11cb20f2e8f
bhimjya <bhimjyanip@gmail.com>
2024-07-21 21:20:14 +0530

    Update pb.ps1

--- .github/workflows/pb.ps1
+++ .github/workflows/pb.ps1
@@ -3,3 +3,4 @@ Get-Date
 "Hello","HELLO" | Select-String -Pattern "HELLO" -CaseSensitive
 30000, 56798, 12432 | ForEach-Object -Process {$_/1024}
 Write-Host "done"
+Get-ChildItem $pshome | ForEach-Object -Process {if (!$_.PSIsContainer) {$_.Name; $_.Length / 1024; " " }}
