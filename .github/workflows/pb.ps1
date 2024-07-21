e929c4c8e6fe520d85314dd6cfc03063f3a955f4
bhimjya <bhimjyanip@gmail.com>
2024-07-21 21:12:32 +0530

    Update pb.ps1

--- .github/workflows/pb.ps1
+++ .github/workflows/pb.ps1
@@ -1,4 +1,4 @@
 Write-Host "Hello, World!"
 Get-Date
-Copy-Item "\\.github\\workflows" -Destination "\\.github\\ISSUE_TEMPLATE" -Recurse
+Copy-Item ".\\.github\\workflows" -Destination ".\\.github\\ISSUE_TEMPLATE" -Recurse
 Write-Host "done"
